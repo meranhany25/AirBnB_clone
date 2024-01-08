24907010d8516fe063c91bfce6938a504aca4473
loda26 <mokhaled26498@gmail.com>
2024-01-08 21:16:48 +0200

    q

--- models/base_model.py
+++ models/base_model.py
@@ -8,18 +8,18 @@ from datetime import datetime
 class BaseModel:
 	def __init__(self, *args, **kwargs):
 		time_format = "%Y-%m-%dT%H:%M:%S.%f"
-		# if kwargs:
-		# 	for key, value in kwargs.items():
-		# 		if key == "__class__":
-		# 			continue
-		# 		elif key == "created_at" or key == "updated_at":
-		# 			setattr(self, key, datetime.strptime(value, time_format))
-		# 		else:
-		# 			setattr(self, key, value)
-		# else:
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.utcnow()
-		self.updated_at = datetime.utcnow()
+		if kwargs:
+			for key, value in kwargs.items():
+				if key == "__class__":
+					continue
+				elif key == "created_at" or key == "updated_at":
+					setattr(self, key, datetime.strptime(value, time_format))
+				else:
+					setattr(self, key, value)
+		else:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.utcnow()
+			self.updated_at = datetime.utcnow()
 
 	def save(self):
 		"""
