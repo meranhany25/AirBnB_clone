c56fe78c34a7587980df80e20ab116dac0143329
loda26 <mokhaled26498@gmail.com>
2024-01-12 15:29:38 +0200

    q

--- console.py
+++ console.py
@@ -4,14 +4,27 @@ Module for console
 """
 import cmd
 import shlex
-from models.base_model import BaseModel
 from models import storage
+from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-    valid_classes = ["BaseModel", "User"]
+    valid_classes = [
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    ]
 
     def emptyline(self):
         """
